b614c37cb2aec925da09d2c2ba90cb2090d581fa
Utkarsh <22utkarshrai@gmail.com>
2025-04-04 07:34:27 +0530

    code for node server added

--- server.js
+++ server.js
@@ -36,4 +36,44 @@ async function sendLocalImage() {
 }
 
 // Run the function
-sendLocalImage();
+
+// Uncomment the code below for node js server to handle image upload from a client
+// and send it to the face cropping service
+// sendLocalImage();
+
+// const express = require("express");
+// const multer = require("multer");
+// const axios = require("axios");
+// const fs = require("fs");
+// const path = require("path");
+// const FormData = require("form-data");
+
+// const app = express();
+// const upload = multer({ dest: "uploads/" });
+
+// app.post("/upload", upload.single("image"), async (req, res) => {
+//     const form = new FormData();
+//     form.append("image", fs.createReadStream(req.file.path));
+
+//     try {
+//         const response = await axios.post("http://127.0.0.1:5001/crop-face", form, {
+//             headers: form.getHeaders(),
+//             responseType: "stream"
+//         });
+
+//         const outputPath = path.join(__dirname, "output", "cropped_from_upload.jpg");
+//         const writer = fs.createWriteStream(outputPath);
+//         response.data.pipe(writer);
+
+//         writer.on("finish", () => {
+//             res.sendFile(outputPath); // Return the cropped image to the user
+//         });
+
+//     } catch (err) {
+//         res.status(500).send("Error processing image: " + err.message);
+//     }
+// });
+
+// app.listen(3000, () => {
+//     console.log("Node.js server running on http://localhost:3000");
+// });
